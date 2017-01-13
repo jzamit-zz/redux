c438d41a69824fb06c340b4d29643b46ac9495c1
jorge <jzamit@gmail.com>
2017-01-12 22:21:05 -0300

    In reducer lessons

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Component } from 'react';
-
+import React, { Component } from 'react';
+//import { Component } from 'react';  //Linea comentada por mi verificar que todoo ande igual.
 
 import BookList from '../containers/book-list';
-
+import BookDetail from '../containers/book-detail';
 
 class App extends Component {
   render() {
     return (
         <div>
             <BookList />
+            <BookDetail />
         </div>
 
     );
---
+++ src/containers/book-detail.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jorge on 10/01/17.
+ */
+
+import React, {Component} from 'react';
+
+class BookDetail extends Component {
+
+    render(){
+
+        return(
+            <div>Book Detail!</div>
+        );
+    }
+
+
+
+}
+
+
+export default BookDetail;
